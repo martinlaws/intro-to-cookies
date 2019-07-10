4848e1edcab1a3ed159f5e1147b20bf117e75970
Martin Laws <martinblaws@gmail.com>
2019-07-09 21:32:51 -0400

    First cookie implementation

--- language-switcher/routes/index.js
+++ language-switcher/routes/index.js
@@ -6,6 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('choose-language');
 });
 
+router.get('/lang/english', function(req, res, next) {
+  res.cookie('language', 'english');
+  res.redirect('/');
+});
 
+router.get('/lang/french', function(req, res, next) {
+  res.cookie('language', 'french');
+  res.redirect('/');
+});
 
 module.exports = router;
