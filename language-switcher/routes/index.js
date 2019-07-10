a6d84f2285afe11a74153f786eb0e24ddb52360e
Martin Laws <martinblaws@gmail.com>
2019-07-09 21:40:46 -0400

    Routing based on cookie content

--- language-switcher/routes/index.js
+++ language-switcher/routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('choose-language');
+  if (req.cookies.language === 'english') {
+    res.render('english')
+  } else if (req.cookies.language === 'french') {
+    res.render('french')
+  } else {
+    res.render('choose-language');
+  }
 });
 
 router.get('/lang/english', function(req, res, next) {
